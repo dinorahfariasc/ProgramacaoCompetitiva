c1b42a75566b91ad8a45beb24dba941804f107ea
Dinorah Farias <dinorahfarias@outlook.com>
2025-03-22 22:13:34 -0300

    exercise 146 neps

---
+++ exercise146.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int X,Y;
+    cin >> X >> Y;
+
+    if(X== 0 || Y == 0){
+        cout << "eixos" << endl;
+    }
+    else if(X > 0 && Y > 0){
+        cout << "Q2" << endl;
+    }
+    else if(X > 0 && Y < 0){
+        cout << "Q4" << endl;
+    }
+    else if(X < 0 && Y > 0){
+        cout << "Q1" << endl;
+    }
+    else if(X < 0 && Y < 0){
+        cout << "Q3" << endl;
+    }
+}
